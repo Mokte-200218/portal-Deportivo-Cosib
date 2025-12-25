ab32512267988eb6bcd388c5eeb3974dec055ae3
Mokte-200218 <emiliohernandezmoctezuma@gmail.com>
2025-12-25 01:01:28 -0600

    Alumnos deporte (#3)
    * Funcionalidad Login Creada
    
    * Primera vista Deportes
    
    * Funcionalidad y vista de alumnos de cada Deporte y boton de regreso en atletas a deportes agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './IU/login/login.component';
+import { DeportesComponent } from './IU/deporte/deporte.component';
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, title: 'Iniciar sesion' },
 
-export const routes: Routes = [];
+  { path: 'deportes', component: DeportesComponent, title: 'Deportes'},
+  {
+  path: 'alumnos/:id',
+  
+  loadComponent: () =>
+    import('./IU/alumnos/alumnos.component')
+      .then(m => m.AlumnosComponent),
+},
+
+  { path: '**', redirectTo: 'login' }
+];
